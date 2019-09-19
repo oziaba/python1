1f55e7bc0121b7779655928ab41621bc29d854f8
oziaba <zdiana099@gmail.com>
2019-09-20 00:32:22 +0900

    Update 2.py

--- 2.py
+++ 2.py
@@ -1,7 +1,7 @@
 #2
 a = input()
 def get_digit_sum(n):
-	return(sum(map(int,n)))
+    return(sum(map(int,n)))
 def is_beauty(n):
-	return int(n) % get_digit_sum(n) == 0
+    return int(n) % get_digit_sum(n) == 0
 print(is_beauty(a))

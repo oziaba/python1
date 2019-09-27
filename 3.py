b9a9657a6b5e1db1341bf7883859babf1db39463
oziaba <zdiana099@gmail.com>
2019-09-27 20:00:56 +0900

    Update 3.py

--- 3.py
+++ 3.py
@@ -1,11 +1,9 @@
 #3
 def is_power_of_two(n):
-    if n == 0:
+    if (n == 0):
         return False
-    while n > 1:
-        if n % 2 == 1:
+    while (n != 1):
+        if (n % 2 != 0):
             return False
-        else:
-            n = n // 2
-            if n == 1:
-                return True
+        n = n // 2
+    return True

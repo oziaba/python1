637273edd2203417b5950a59ce91b6daa4171583
oziaba <zdiana099@gmail.com>
2019-09-27 13:57:06 +0900

    Update 4.py

--- 4.py
+++ 4.py
@@ -1,9 +1,8 @@
 #4
-a = input()
 def is_prime(n):
-	n = int(n)
-	d = 2
-	while n % d != 0:
-		d += 1
-	return d == n
-print(is_prime(a))
+	if n == 1:
+        return True
+    d = 2
+    while n % d != 0:
+        d += 1
+    return d == n

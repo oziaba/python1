db42f4e12cc20d111395beac2ddce369fb6eeae6
oziaba <zdiana099@gmail.com>
2019-09-28 13:59:56 +0900

    Update 5.py

--- 5.py
+++ 5.py
@@ -5,7 +5,7 @@ def is_self_dividing(n):
     for i in str(n):
         if n % int(i) == 0:
             d += 1
-        if d == k:
-            return True
-        else:
-            return False
+    if d == k:
+        return True
+    else:
+        return False

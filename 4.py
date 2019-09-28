83d00bcfcfe4b8e44ef30432939d0d7c76bc4e95
oziaba <zdiana099@gmail.com>
2019-09-28 14:11:39 +0900

    Update 4.py

--- 4.py
+++ 4.py
@@ -2,7 +2,7 @@
 def is_prime(n):
     if n == 1:
         return True
-    d = 2
-    while n % d != 0:
-        d += 1
-        return d == n
+    for i in range(2, round(n/2)+1):
+        if n % i == 0:
+            return False
+    return True

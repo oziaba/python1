86a3efe19e7938c4c05b0d85f2dc17b926985b26
oziaba <zdiana099@gmail.com>
2019-10-02 22:04:01 +0900

    Delete from remote repository

--- 3.py
+++
@@ -1,9 +0,0 @@
-#3
-def is_power_of_two(n):
-    if (n == 0):
-        return False
-    while (n != 1):
-        if (n % 2 != 0):
-            return False
-        n = n // 2
-    return True
--- 4.py
+++
@@ -1,8 +0,0 @@
-#4
-def is_prime(n):
-    if n == 1:
-        return True
-    for i in range(2, round(n/2)+1):
-        if n % i == 0:
-            return False
-    return True
